dd28ec5b8363a51c93cb75f435504095468757fd
Devon <dball@chapman.edu>
2018-02-06 23:10:04 +0000

    Added module description

--- coord.py
+++ coord.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""Module Description:
+Module contains function coord_for(n, a=0, b=0) that takes a number of intervals and two 
+end points of a segment and returns a list of the interval division coordinates.
+
+Arguments:
+n - the number of intervals
+a - lower segment bound that has been given a default value of a=0
+b - upper segment bound that has been given a default value of b=0
+"""
 def coord_for(n, a=0, b=1):
     coords = []
     int_l = b-a
