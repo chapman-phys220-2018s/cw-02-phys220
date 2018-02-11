79210a4575ce4d93f45588b147115d2df224975d
Daniel <Chang254@mail.chapman.edu>
2018-02-11 22:18:41 +0000

    added generator

--- fibs.py
+++ fibs.py
@@ -8,3 +8,16 @@ def fibs(n):
         numbers.append(numbers[k-1]+numbers[k])
         k+=1
     return numbers
+
+def fib_generator():
+    a = 0
+    b = 1
+    c = 1
+    while True:
+        yield c
+        c = a + b
+        a=b
+        b=c
+
+
+
